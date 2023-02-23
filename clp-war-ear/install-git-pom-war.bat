b8a9ea55784fcd1126585e11bc9a204336924818
Benny <wen-ying.external@atos.net>
2023-02-23 09:18:04 +0800

    打包配置

--- clp-war-ear/install-git-pom-war.bat
+++ clp-war-ear/install-git-pom-war.bat
@@ -1,4 +1,4 @@
 @echo off
 
 echo %M2_HOME%\bin\mvn -f pom-ear.xml clean install -Dmaven.test.skip=true -D%Package_type% -DfinalName=%JOB_NAME%-%dlsVersion%.stable-%buildTime% -DbuildTime=%buildTime% -DdlsVersion=%dlsVersion% -Drun.env=%package_env% -DCOOKIE-NAME=%COOKIE-NAME% -l log-ear.log 
-%M2_HOME%\bin\mvn -f pom-ear.xml clean install -Dmaven.test.skip=true -D%Package_type% -DfinalName=%JOB_NAME%-%dlsVersion%.stable-%buildTime% -DbuildTime=%buildTime% -DdlsVersion=%dlsVersion% -Drun.env=%package_env% -DCOOKIE-NAME=%COOKIE-NAME% -l log-ear.log 
+%M2_HOME%\bin\mvn -f pom-ear.xml clean install -Dmaven.test.skip=true -D%Package_type% -DfinalName=%JOB_NAME%-%clpVersion%.stable-%buildTime% -DbuildTime=%buildTime% -DclpVersion=%clpVersion% -Drun.env=%package_env% -DCOOKIE-NAME=%COOKIE-NAME% -l log-ear.log 
